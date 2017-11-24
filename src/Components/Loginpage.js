88fb476fd7af6287dc91664461feb89a9d5154ec
Kelvan Ince <incekelvan@gmail.com>
2017-11-24 15:10:26 -0600

    Four primary components of app

---
+++ src/Components/Loginpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Loginpage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Loginpage</h3>
+      </div>
+    )
+  }
+}
+
+export default Loginpage;
---
+++ src/Components/Skillspage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SkillsPage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>SkillsPage</h3>
+      </div>
+    )
+  }
+}
+
+export default SkillsPage;
