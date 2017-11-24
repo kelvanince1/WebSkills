55635bb02a6f715521bc19fedcd699421ee7cc64
Kelvan Ince <incekelvan@gmail.com>
2017-11-24 15:41:03 -0600

    React bootstrap

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar, NavItem, Nav} from 'react-bootstrap';
 
 class Homepage extends Component {
   render() {
     return (
+      <Navbar>
+        <Nav>
+          <NavItem>Home</NavItem>
+          <NavItem>Skills</NavItem>
+        </Nav>
+      </Navbar>,
       <div>
         <h3>Hello Username</h3>
         <h4>See your skills or endorse someone elses</h4>
