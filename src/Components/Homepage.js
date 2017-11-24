c8c889594399463d3c0a5f17d82b6910f79cb578
Kelvan Ince <incekelvan@gmail.com>
2017-11-24 15:14:04 -0600

    Removed unnecessary boiler files

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -4,7 +4,8 @@ class Homepage extends Component {
   render() {
     return (
       <div>
-        <h3>Homepage</h3>
+        <h3>Hello Username</h3>
+        <h4>See your skills or endorse someone elses</h4>
       </div>
     )
   }
