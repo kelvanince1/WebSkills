2ccef9d27f4ae32936494f184c5393bc5ac3faea
Kelvan Ince <incekelvan@gmail.com>
2017-11-25 10:31:24 -0600

    Header component

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {Button } from 'react-bootstrap';
 
+import Header from './Header';
+
 // I am using React-Bootstrap for some styling.
 // Url: https://react-bootstrap.github.io/components.html
 
@@ -10,6 +12,7 @@ class Homepage extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h3>Hello Username</h3>
         <h4>See your skills or endorse someone elses</h4>
         <Button><Link to="/skills">Skills</Link></Button>
