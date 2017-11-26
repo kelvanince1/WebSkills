38b1ea2798c1414b108cb15cdf6600be0d5cfea5
Kelvan Ince <incekelvan@gmail.com>
2017-11-26 14:28:54 -0600

    More form modifications

--- src/Components/NewSkills.js
+++ src/Components/NewSkills.js
@@ -1,54 +1,65 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
-// import AWS from 'aws-sdk';
-// import {Button } from 'react-bootstrap';
+import AWS from 'aws-sdk';
 // import { Link } from 'react-router-dom';
 //
 import Header from './Header';
-//
-// var ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
-//
-// AWS.config.credentials = new AWS.Credentials({accessKey: process.env.ACCESS_KEY, secretKey: process.env.SECRET_KEY});
+
+AWS.config.update({
+  region: 'us-west-1'
+});
+
+AWS.config.credentials = new AWS.Credentials({accessKey: process.env.ACCESS_KEY, secretKey: process.env.SECRET_KEY});
+
+var ddb = new AWS.DynamoDB.DocumentClient();
 
 class NewSkills extends Component {
-  // handleSubmit = () => {
-  //   console.log()
-  //   var params = {
-  //     TableName: 'Skills',
-  //     Item: {
-  //       'UserId' : this.state.username,
-  //       'Skill': this.state.skill
-  //     }
-  //   };
-  //
-  //   ddb.putItem(params, function(err, data) {
-  //     if (err) {
-  //       console.log("Error", err);
-  //     } else {
-  //       console.log("Success", data);
-  //     }
-  //   });
-  // };
+  constructor(props) {
+    super(props);
 
-  // handleUsername = (event) => this.setState({username: event.target.value});
-  // handleSkill = (event) => this.setState({skill: event.value});
+    this.state = {
+      username: '',
+      skill: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  handleSubmit = () => {
+    var params = {
+      TableName: 'Skills',
+      Item: {
+        'UserId' : this.state.username,
+        'Skill': this.state.skill
+      }
+    };
+
+    ddb.putItem(params, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data);
+      }
+    });
+  };
 
   render() {
+    const { fields: { username, skill }, handleSubmit } = this.props;
     return (
       <div>
         <Header />
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <h3>New Skill</h3>
           <div className='form-group'>
             <label>Username</label>
-            <input type='text' className='form-control' />
+            <input type='text' className='form-control' {...this.state.username} />
           </div>
 
           <div className='form-group'>
             <label>Skill</label>
-            <input type='text' className='form-control' />
+            <input type='text' className='form-control' {...this.state.skill} />
           </div>
-          <button type='submit' className='btn btn-primary'>Submit</button>
+          <button onSubmit={this.handleSubmit} type='submit' className='btn btn-primary'>Submit</button>
         </form>
       </div>
     );
@@ -56,6 +67,6 @@ class NewSkills extends Component {
 }
 
 export default reduxForm({
-  form: 'skillsNewForm',
-  fields: ['username', 'skills']
+  form: 'SkillsNewForm',
+  fields: ['username', 'skill']
 })(NewSkills);
