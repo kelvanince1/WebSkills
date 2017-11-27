834fe1d727e25aa7c06c6034bfcf52c90e6c2741
Kelvan Ince <incekelvan@gmail.com>
2017-11-26 18:12:57 -0600

    Action creator for fetching skills data from Beanstalk endpoint

--- src/Actions/Skills.js
+++ src/Actions/Skills.js
@@ -1,7 +1,14 @@
-import { FETCH_SKILLS } from './ActionTypes';
+import { SKILLS_DATA } from './ActionTypes';
 
-export function fetchSkills() {
-  return {
-    type: FETCH_SKILLS
-  }
+// Asynchronously await updates in the URL endpoint. When a new item is added,
+// grab it and pull it into the app.
+
+// This data will be passed through to the components as State.
+export const skillsData = () => async dispatch => {
+  const resWithId = await fetch(``);
+  // const json = await res.json();
+  const jsonWithId = await resWithId.json();
+  const jsonWithIdShift = jsonWithId.Items.shift();
+
+  // dispatch({type: actionTypes.SKILLS_DATA, payload: jsonWithId});
 }
