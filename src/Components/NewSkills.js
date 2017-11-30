481d9a40686e0e90a0e8b40b83608a5266f717e4
Kelvan Ince <incekelvan@gmail.com>
2017-11-29 16:22:02 -0800

    Form for submitting new skills

--- src/Components/NewSkills.js
+++ src/Components/NewSkills.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
-// import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 // import { Link } from 'react-router-dom';
 //
 import Header from './Header';
 
 class NewSkills extends Component {
+  renderSkillField(field) {
+    return (
+      <div>
+        <input
+          type='text'
+          {...field.input}
+        />
+      </div>
+    )
+  };
+
   render() {
     return (
       <div>
         <Header />
         <form>
-
+          <Field
+            name='Skill'
+            component={this.renderSkillField}
+          />
         </form>
       </div>
     );
   }
 }
 
-export default NewSkills;
+export default reduxForm({
+  form: 'SkillsNewForm'
+})(NewSkills);
 // export default reduxForm({
 //   form: 'SkillsNewForm',
 //   fields: ['username', 'skill']
