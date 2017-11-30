716bde49c0d78e1b112a9c8a6a22f13dba4358e9
Kelvan Ince <incekelvan@gmail.com>
2017-11-30 11:33:39 -0800

    NewSkills component sending data to DynamoDB

--- src/Components/NewSkills.js
+++ src/Components/NewSkills.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
+import AWS from 'aws-sdk';
 // import { Link } from 'react-router-dom';
 //
 import Header from './Header';
+
+AWS.config.update({
+  region: 'us-east-1'
+});
+
+AWS.config.credentials = new AWS.Credentials({accessKeyId: process.env.REACT_APP_ACCESS_KEY, secretAccessKey: process.env.REACT_APP_SECRET_KEY});
+
+var ddb = new AWS.DynamoDB.DocumentClient();
 
 class NewSkills extends Component {
   renderField(field) {
@@ -14,15 +23,43 @@ class NewSkills extends Component {
           type='text'
           {...field.input}
         />
+        {field.meta.error}
       </div>
     )
   };
 
+  onSubmit(values) {
+    console.log(values);
+    var dynamoParams = {
+      TableName: 'Skills',
+      Item: {
+        "username": values.Username,
+        "skills": values.Skill
+      }
+    }
+    ddb.put(dynamoParams, function(err, data) {
+         if (err) {
+           console.log("Error", err);
+         } else {
+           console.log("Success", data);
+         }
+       });
+
+     ddb.update(dynamoParams, function(err, data) {
+       if (err) {
+         console.log("Error", err);
+       } else {
+         console.log("Success", data);
+       }
+     });
+  }
+
   render() {
+    const { handleSubmit } = this.props;
     return (
       <div>
         <Header />
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <Field
             label='Username'
             name='Username'
@@ -33,16 +70,27 @@ class NewSkills extends Component {
             name='Skill'
             component={this.renderField}
           />
+          <button type='submit' className='btn btn-primary'>Save</button>
         </form>
       </div>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.Username) {
+    errors.Username = "Enter your username";
+  }
+  if (!values.Skill) {
+    errors.Skill = "Enter a new skill";
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'SkillsNewForm'
 })(NewSkills);
-// export default reduxForm({
-//   form: 'SkillsNewForm',
-//   fields: ['username', 'skill']
-// }, null, { createSkill })(NewSkills);
